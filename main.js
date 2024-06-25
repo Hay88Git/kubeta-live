ca87227883ff7fae54f88f46f071f91b3c9c5830
Hay88Git <myvercel2024@gmail.com>
2024-06-25 11:25:32 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let Nav = document.getElementById("nav");
+let OpenNav = function(){
+  Nav.style.left = "0%";
+}
+let CloseNav = function(){
+  Nav.style.left = "-100%";
+}
+
+function Link(){
+  location.href = "https://www.hay88.one/?inviteCode=7226257";
+}
